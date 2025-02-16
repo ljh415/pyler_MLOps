0c7fc6a7076538861693b3472d96f9fc08809d9a
ljh415 <ljh415@gmail.com>
2025-02-17 02:35:02 +0900

    FEAT: add logger, using export_utils.py, add model comparison process, add metadata to onnx

--- training/export.py
+++ training/export.py
@@ -1,20 +1,54 @@
 import os
+import logging
+
 import torch
 import torch.nn as nn
 from torchvision.models import resnet18
 
-def export(target_type):
-    
-    save_path = os.environ.get("SAVE_PATH", "/storage")
-    model_save_path = os.path.join(save_path, "model.pth")
-    onnx_save_path = os.path.join(save_path, "models", "resnet18_mnist", "1", "model.onnx")  # onnx로 변환은 export.py에서 진행할 예정
+from export_utils import find_best_model, update_best_model_symlink, get_model_info, get_next_model_version, add_metadata_to_onnx
 
-    print("Model init..", flush=True)
+def export(target_type):
+    save_path = os.environ.get("SAVE_PATH", "/storage")
+    model_dir = os.path.join(save_path, "torch_models")
+    model_repo_path = os.path.join(save_path, "models", "resnet18_mnist")
+    os.makedirs(model_repo_path, exist_ok=True)
+
+    log_messages = []
+    ## best acc model check
+    best_model = find_best_model(model_dir)
+    if not best_model:
+        log_messages.append("No model found in torch_models directory")
+        return
+    
+    ## symlink update
+    target_model_path = update_best_model_symlink(model_dir, best_model, log_messages)
+    if target_model_path is None:
+        log_messages.append("Export process skipped as best model remains unchanged.")
+        return
+    
+    ## get model info
+    model_timestamp, model_acc = get_model_info(target_model_path)
+    
+    log_file = os.path.join(save_path, "logs", f"{model_timestamp}", "export.log")
+    os.makedirs(os.path.dirname(log_file), exist_ok=True)
+    logging.basicConfig(filename=log_file, level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+    logger = logging.getLogger()
+    
+    for msg in log_messages:
+        logger.info(msg)
+    
+    logger.info(f"Using model: {target_model_path}")
+    
+    new_version = get_next_model_version(model_repo_path)
+    onnx_save_path = os.path.join(model_repo_path, new_version, "model.onnx")
+    
+    ## model load
+    logger.info("Loading model...")
     if target_type=="onnx":
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         dummy_input = torch.randn(1, 1, 28, 28).to(device)
         
-        ckpt = torch.load(model_save_path, map_location=device, weights_only=False)
+        ckpt = torch.load(target_model_path, map_location=device, weights_only=False)
         
         if isinstance(ckpt, dict):
             model = resnet18(pretrained=False)
@@ -38,7 +72,15 @@ def export(target_type):
             dynamic_axes={"input": {0: "batch_size"}, "output": {0: "batch_size"}},
             opset_version=11
         )
-        print(f"ONNX Model saved to {onnx_save_path}", flush=True)
+        logger.info(f"ONNX Model saved to {onnx_save_path}")
+        
+        model_metadata = {
+            "export_date": model_timestamp,
+            "accuracy": model_acc,
+            "source_model": target_model_path
+        }
+        add_metadata_to_onnx(onnx_save_path, model_metadata)
+        
     else :
         raise Exception("Not supported export type")
 
